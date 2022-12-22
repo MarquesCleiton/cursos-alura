42eee9317ae7b624de6dc620a6ed7c2869eed549
marquescleiton <cleiton.contato2210@gmail.com>
2022-12-21 21:53:43 -0300

    finalização curso Spring Boot 3 - aplique boas práticas e proteja uma API Rest

---
+++ Spring Boot 3 - aplique boas práticas e proteja uma API Rest/api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,47 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository repository;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenJWT = recuperarToken(request);
+
+        if(tokenJWT != null){ // Só valide o token se for passado
+            String subject = tokenService.getSubject(tokenJWT);
+            UserDetails usuario = repository.findByLogin(subject);
+
+            Authentication authetication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authetication);
+
+        }
+        filterChain.doFilter(request, response);
+    }
+    private String recuperarToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader != null){ //Se ninguém passar o token, devolva nulo
+            return authorizationHeader.replace("Bearer ","");
+        }
+        return null;
+    }
+}
